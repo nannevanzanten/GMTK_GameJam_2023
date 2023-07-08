b0d6f221d9518ab28db4ac9efe3b2afaf8ed12b4
martijnreus <martijn.reus@gmail.com>
2023-07-08 19:42:35 +0200

    The tree have now a shoot cool down

--- GMTK_GameJam/Assets/Scripts/Runtime/Tree.cs
+++ GMTK_GameJam/Assets/Scripts/Runtime/Tree.cs
@@ -4,54 +4,29 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    [SerializeField] private Sprite[] treeSprites;
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    public bool canShoot = true;
 
-    [SerializeField] private float timeToLevel1;
-    [SerializeField] private float timeToLevel2;
-
-    [SerializeField] private int treeLevel = 0;
-
-    private float timeOfPlanting;
-
-    private bool isPlanted = false;
+    [SerializeField] private float timeBetweenShooting;
+    private float shootTime;
 
     private void Update()
     {
-        // Only runs once when planted
-        if (!isPlanted)
+        if (!canShoot)
         {
-            timeOfPlanting = Time.time;
-            SetSortingLayer();
-            isPlanted = true;
+            resetTimer();
         }
-
-        GrowTree();
+        else if (canShoot)
+        {
+            shootTime = Time.time;
+        }
     }
 
-    private void UpdateTreeSprite()
+    private void resetTimer()
     {
-        spriteRenderer.sprite = treeSprites[treeLevel];
-    }
-
-    private void GrowTree()
-    {
-        if (Time.time - timeOfPlanting >= timeToLevel1 && Time.time - timeOfPlanting < timeToLevel2)
+        Debug.Log(Time.time - shootTime);
+        if (Time.time - shootTime > timeBetweenShooting)
         {
-            treeLevel = 1;
-            UpdateTreeSprite();
+            canShoot = true;
         }
-        else if (Time.time - timeOfPlanting >= timeToLevel2)
-        {
-            treeLevel = 2;
-            UpdateTreeSprite();
-        }
-
-    }
-
-    private void SetSortingLayer()
-    {
-        int order = -(int)(transform.position.y * 100);
-        spriteRenderer.sortingOrder = order;
     }
 }
