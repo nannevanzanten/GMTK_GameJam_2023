08a6fbd2fd0515c76f9e971fdd9659a08129d6a1
martijnreus <martijn.reus@gmail.com>
2023-07-09 12:30:07 +0200

    Now added daynight cycle

--- GMTK_GameJam/Assets/Scripts/Runtime/GameManager.cs
+++ GMTK_GameJam/Assets/Scripts/Runtime/GameManager.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private TimeCycle timeCycle;
+    private LumberjackSpawnPoint lumberjackSpawnPoint;
+
     private int lumberJacksToSpawn;
 
-     
+    private void Awake()
+    {
+        timeCycle = FindObjectOfType<TimeCycle>();
+        lumberjackSpawnPoint = FindObjectOfType<LumberjackSpawnPoint>();
+
+        timeCycle.OnStartDay += TimeCycle_OnStartDay;
+    }
+
+    private void TimeCycle_OnStartDay(object sender, System.EventArgs e)
+    {
+        lumberJacksToSpawn = timeCycle.GetDayCount();
+        lumberjackSpawnPoint.SpawnLumberJacks(lumberJacksToSpawn);
+    }
+
+
 
 }
