b56907b8d77f67f5a679d03b4db95add8258fe10
Nanne <27259@ma-web.nl>
2023-07-09 13:07:39 +0200

    day n nite cycle added

--- GMTK_GameJam/Assets/Scripts/Runtime/UI.cs
+++ GMTK_GameJam/Assets/Scripts/Runtime/UI.cs
@@ -1,10 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
-
-[SerializedField] 
 
 public class UI : MonoBehaviour
 {
