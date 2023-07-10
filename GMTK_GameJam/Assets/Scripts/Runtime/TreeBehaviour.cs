e1561a567f0c442eb49283622f317daf2eca57eb
martijnreus <martijn.reus@gmail.com>
2023-07-10 14:26:15 +0200

    Added webGL

--- GMTK_GameJam/Assets/Scripts/Runtime/TreeBehaviour.cs
+++ GMTK_GameJam/Assets/Scripts/Runtime/TreeBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TreeBehaviour : MonoBehaviour
 {
-    public bool canShoot = true;
+    public bool canShoot = false;
     public bool isDead = false;
 
     public AudioClip _throwNut;
@@ -12,6 +12,7 @@ public class TreeBehaviour : MonoBehaviour
     public AudioSource _nutSource;
 
     [SerializeField] private float timeBetweenShooting;
+    [SerializeField] private GameObject acorn;
     private float shootTime;
 
     public int Health;
@@ -25,10 +26,16 @@ public class TreeBehaviour : MonoBehaviour
     {
         if (!canShoot)
         {
+            HideAcorn();
             ResetTimer();
         }
         else if (canShoot)
         {
+            if (shootTime != 0)
+            {
+                ShowAcorn();
+            }
+            
             shootTime = Time.time;
         }
 
@@ -51,5 +58,25 @@ public class TreeBehaviour : MonoBehaviour
         isDead = true;
         yield return new WaitForSeconds(0.001f);
         Destroy(gameObject);
+    }
+
+    private void ShowAcorn()
+    {
+        acorn.SetActive(true);
+        /*
+        if (canShoot)
+        {
+            acorn.SetActive(true);
+        }
+        else
+        {
+            acorn.SetActive(false);
+        }
+        */
+    }
+
+    private void HideAcorn()
+    {
+        acorn.SetActive(false);
     }
 }
